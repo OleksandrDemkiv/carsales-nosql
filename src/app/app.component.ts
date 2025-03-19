a7c97be6e54720c15a65d93dd9fb38cce9225280
OleksandrDemkiv <s00251154@atu.ie>
2025-03-19 12:03:27 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
+import { CarlistComponent } from './components/carlist/carlist.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CarlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
